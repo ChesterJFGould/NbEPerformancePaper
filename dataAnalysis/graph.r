e01ec5ee2cbe4df7485d47cf119193f17111c741
kiana47 <kiana47@student.ubc.ca>
2025-06-09 17:07:10 -0700

    fixed typos

--- dataAnalysis/graph.r
+++ dataAnalysis/graph.r
@@ -28,11 +28,12 @@ merged_data <- transform(
   data.results.mean.x = 1)
 
 plot <- ggplot(merged_data, aes(name, data.results.mean.x)) +
-  geom_col(aes(fill = "syntax directed"), width = 0.45, position = position_nudge(x = -0.225)) +
+  geom_col(aes(fill = "syntax-directed"), width = 0.45, position = position_nudge(x = -0.225)) +
   geom_errorbar(aes(ymin = -2*data.results.stddev.x + data.results.mean.x, ymax = 2*data.results.stddev.x + data.results.mean.x), width = 0.45, position = position_nudge(x = -0.225)) +
-  geom_col(aes(y = data.results.mean.y, fill = "type directed"), width = 0.45, position = position_nudge(x = 0.225)) +
+  geom_col(aes(y = data.results.mean.y, fill = "type-directed"), width = 0.45, position = position_nudge(x = 0.225)) +
   geom_errorbar(aes(ymin = -2*data.results.stddev.y + data.results.mean.y, ymax = 2*data.results.stddev.y + data.results.mean.y), width = 0.45, position = position_nudge(x = 0.225)) +
   theme(text = element_text(size = 20), axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1)) +
   labs(x = "benchmark", y = "relative mean time", fill = "implementation")
 
 normalized_mean_type_time <- mean(filter(merged_data, data.results.mean.y != Inf)$data.results.mean.y)
+ggsave("benchmark_graph.png", plot=last_plot())
